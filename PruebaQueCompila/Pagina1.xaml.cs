5e4a68175265800768a890b36c0daa66c0ba2113
Juan Sebastian Cadena Ramirez <juan.cadena.ramirez@udla.edu.ec>
2025-01-20 21:36:28 -0500

    organizacion de componentes y funciones

--- PruebaQueCompila/Pagina1.xaml.cs
+++ PruebaQueCompila/Pagina1.xaml.cs
@@ -1,104 +1,189 @@
+using SQLite;
+
+using System.Net.Http;
+
+using Newtonsoft.Json;
+
+using System.IO;
+
+using Microsoft.Maui.Storage;
+
+
+
 namespace PruebaQueCompila
+
 {
-    using SQLite;
-    using System.Net.Http;
-    using Newtonsoft.Json;
-    using System.IO;
-    using Microsoft.Maui.Storage;
 
     public partial class Pagina1 : ContentPage
+
     {
+
         private SQLiteAsyncConnection basedatos;
-namespace PruebaQueCompila
-    {
-        using SQLite;
-        using System.Net.Http;
-        using Newtonsoft.Json;
-        using System.IO;
-        using Microsoft.Maui.Storage;
 
-        public partial class Pagina1 : ContentPage
+
+
+        public Pagina1()
+
         {
-            private SQLiteAsyncConnection basedatos;
 
-            public Pagina1()
-            {
-                InitializeComponent();  // Asegúrate de que este método esté presente
-                conexionbd();  
-            }
+            InitializeComponent();
 
-            private async void conexionbd()
-            {
-                string rutaBD = Path.Combine(FileSystem.AppDataDirectory, "Base.db3");
-                basedatos = new SQLiteAsyncConnection(rutaBD);
-                await basedatos.CreateTableAsync<Pais>(); // Crea la tabla de países
-            }
+            conexionbd();
 
-            public class Pais
-            {
-                [PrimaryKey, AutoIncrement]
-                public int Id { get; set; }
-                public string NombreOficial { get; set; }
-                public string Region { get; set; }
-                public string GoogleMapsLink { get; set; }
-            }
-
-            
-            public async Task BuscarPais(string nombrePais)
-            {
-                var client = new HttpClient();
-                var url = $"https://restcountries.com/v3.1/name/{nombrePais}?fields=name,region,maps";
-                var response = await client.GetStringAsync(url);
-                var paises = JsonConvert.DeserializeObject<List<Pais>>(response);
-
-                if (paises != null && paises.Count > 0)
-                {
-                    var pais = paises[0];
-                    GuardarPais(pais); 
-                    MostrarPais(pais); 
-                }
-                else
-                {
-                    await DisplayAlert("Error", "No se encontró el país", "OK");
-                }
-            }
-
-            
-            public async void GuardarPais(Pais pais)
-            {
-                await basedatos.InsertAsync(pais); 
-            }
-
-            
-            private void MostrarPais(Pais pais)
-            {
-                labelPais.Text = $"Nombre: {pais.NombreOficial}\n" +
-                                 $"Región: {pais.Region}\n" +
-                                 $"Google Maps: {pais.GoogleMapsLink}";
-            }
-
-            
-            private async void OnBuscarClicked(object sender, EventArgs e)
-            {
-                var nombrePais = entryNombrePais.Text; 
-                if (!string.IsNullOrEmpty(nombrePais))
-                {
-                    await BuscarPais(nombrePais);
-                }
-            }
-
-            // Evento del botón "Limpiar"
-            private void OnLimpiarClicked(object sender, EventArgs e)
-            {
-                entryNombrePais.Text = string.Empty;  
-                labelPais.Text = string.Empty;        
-            }
         }
+
+
+
+        private async void conexionbd()
+
+        {
+
+            string rutaBD = Path.Combine(FileSystem.AppDataDirectory, "Base.db3");
+
+            basedatos = new SQLiteAsyncConnection(rutaBD);
+
+            await basedatos.CreateTableAsync<Pais>();
+
+        }
+
+
+
+        public class Pais
+
+        {
+
+            [PrimaryKey, AutoIncrement]
+
+            public int Id { get; set; }
+
+            public string NombreOficial { get; set; }
+
+            public string Region { get; set; }
+
+            public string GoogleMapsLink { get; set; }
+
+        }
+
+
+
+        public async Task BuscarPais(string nombrePais)
+
+        {
+
+            var client = new HttpClient();
+
+            var url = $"https://restcountries.com/v3.1/name/{nombrePais}?fields=name,region,maps";
+
+            try
+
+            {
+
+                var response = await client.GetStringAsync(url);
+
+                var paisesJson = JsonConvert.DeserializeObject<List<dynamic>>(response);
+
+
+
+                if (paisesJson != null && paisesJson.Count > 0)
+
+                {
+
+                    var paisJson = paisesJson[0];
+
+                    var pais = new Pais
+
+                    {
+
+                        NombreOficial = paisJson.name.official,
+
+                        Region = paisJson.region,
+
+                        GoogleMapsLink = paisJson.maps.googleMaps
+
+                    };
+
+
+
+                    GuardarPais(pais);
+
+                    MostrarPais(pais);
+
+                }
+
+                else
+
+                {
+
+                    await DisplayAlert("Error", "No se encontró el país", "OK");
+
+                }
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                await DisplayAlert("Error", $"Ocurrió un error: {ex.Message}", "OK");
+
+            }
+
+        }
+
+
+
+        public async void GuardarPais(Pais pais)
+
+        {
+
+            await basedatos.InsertAsync(pais);
+
+        }
+
+
+
+        private void MostrarPais(Pais pais)
+
+        {
+
+            labelPais.Text = $"Nombre: {pais.NombreOficial}\n" +
+
+                             $"Región: {pais.Region}\n" +
+
+                             $"Google Maps: {pais.GoogleMapsLink}";
+
+        }
+
+
+
+        private async void OnBuscarClicked(object sender, EventArgs e)
+
+        {
+
+            var nombrePais = entryNombrePais.Text;
+
+            if (!string.IsNullOrEmpty(nombrePais))
+
+            {
+
+                await BuscarPais(nombrePais);
+
+            }
+
+        }
+
+
+
+        private void OnLimpiarClicked(object sender, EventArgs e)
+
+        {
+
+            entryNombrePais.Text = string.Empty;
+
+            labelPais.Text = string.Empty;
+
+        }
+
     }
-
-
-
-
-
 
 }
