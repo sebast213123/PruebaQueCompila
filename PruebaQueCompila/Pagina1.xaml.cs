ffd366b1b97a757630225a5afdb436105999df32
Juan Sebastian Cadena Ramirez <juan.cadena.ramirez@udla.edu.ec>
2025-01-20 21:08:01 -0500

    proyecto integrado

--- PruebaQueCompila/Pagina1.xaml.cs
+++ PruebaQueCompila/Pagina1.xaml.cs
@@ -1,20 +1,104 @@
-namespace PruebaQueCompila;
-using SQLite;
-public partial class Pagina1 : ContentPage
+namespace PruebaQueCompila
 {
-	public Pagina1()
-	{
+    using SQLite;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using System.IO;
+    using Microsoft.Maui.Storage;
 
-		InitializeComponent();
-	}
-
-    public class Pais
+    public partial class Pagina1 : ContentPage
     {
-        [PrimaryKey, AutoIncrement] 
-        public int Id { get; set; }
-        public string NombreOficial { get; set; }
-        public string Region { get; set; }
-        public string GoogleMapsLink { get; set; }
+        private SQLiteAsyncConnection basedatos;
+namespace PruebaQueCompila
+    {
+        using SQLite;
+        using System.Net.Http;
+        using Newtonsoft.Json;
+        using System.IO;
+        using Microsoft.Maui.Storage;
+
+        public partial class Pagina1 : ContentPage
+        {
+            private SQLiteAsyncConnection basedatos;
+
+            public Pagina1()
+            {
+                InitializeComponent();  // Asegúrate de que este método esté presente
+                conexionbd();  
+            }
+
+            private async void conexionbd()
+            {
+                string rutaBD = Path.Combine(FileSystem.AppDataDirectory, "Base.db3");
+                basedatos = new SQLiteAsyncConnection(rutaBD);
+                await basedatos.CreateTableAsync<Pais>(); // Crea la tabla de países
+            }
+
+            public class Pais
+            {
+                [PrimaryKey, AutoIncrement]
+                public int Id { get; set; }
+                public string NombreOficial { get; set; }
+                public string Region { get; set; }
+                public string GoogleMapsLink { get; set; }
+            }
+
+            
+            public async Task BuscarPais(string nombrePais)
+            {
+                var client = new HttpClient();
+                var url = $"https://restcountries.com/v3.1/name/{nombrePais}?fields=name,region,maps";
+                var response = await client.GetStringAsync(url);
+                var paises = JsonConvert.DeserializeObject<List<Pais>>(response);
+
+                if (paises != null && paises.Count > 0)
+                {
+                    var pais = paises[0];
+                    GuardarPais(pais); 
+                    MostrarPais(pais); 
+                }
+                else
+                {
+                    await DisplayAlert("Error", "No se encontró el país", "OK");
+                }
+            }
+
+            
+            public async void GuardarPais(Pais pais)
+            {
+                await basedatos.InsertAsync(pais); 
+            }
+
+            
+            private void MostrarPais(Pais pais)
+            {
+                labelPais.Text = $"Nombre: {pais.NombreOficial}\n" +
+                                 $"Región: {pais.Region}\n" +
+                                 $"Google Maps: {pais.GoogleMapsLink}";
+            }
+
+            
+            private async void OnBuscarClicked(object sender, EventArgs e)
+            {
+                var nombrePais = entryNombrePais.Text; 
+                if (!string.IsNullOrEmpty(nombrePais))
+                {
+                    await BuscarPais(nombrePais);
+                }
+            }
+
+            // Evento del botón "Limpiar"
+            private void OnLimpiarClicked(object sender, EventArgs e)
+            {
+                entryNombrePais.Text = string.Empty;  
+                labelPais.Text = string.Empty;        
+            }
+        }
     }
+
+
+
+
+
 
 }
--- PruebaQueCompila/Pagina2.xaml.cs
+++ PruebaQueCompila/Pagina2.xaml.cs
@@ -1,9 +1,62 @@
-namespace PruebaQueCompila;
-
-public partial class Pagina2 : ContentPage
+namespace PruebaQueCompila
 {
-	public Pagina2()
-	{
-		InitializeComponent();
-	}
+    using SQLite;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.Maui.Controls;
+    using System.IO;
+
+    public partial class Pagina2 : ContentPage
+    {
+        private SQLiteAsyncConnection basedatos;
+
+        public Pagina2()
+        {
+            InitializeComponent();
+            conexionbd(); 
+            LeerPaises(); 
+        }
+
+      
+        private async void conexionbd()
+        {
+            string rutaBD = Path.Combine(FileSystem.AppDataDirectory, "Base.db3");
+            basedatos = new SQLiteAsyncConnection(rutaBD);
+            await basedatos.CreateTableAsync<Pais>(); 
+        }
+
+        
+        private async void LeerPaises()
+        {
+            var paises = await basedatos.Table<Pais>().ToListAsync(); 
+
+            var paisesList = new List<PaisDisplay>();
+
+            foreach (var pais in paises)
+            {
+                paisesList.Add(new PaisDisplay
+                {
+                    DisplayText = $"Nombre País: {pais.NombreOficial}, Región: {pais.Region}, Link: {pais.GoogleMapsLink}, NombreBD: Scadena"
+                });
+            }
+
+            listViewPaises.ItemsSource = paisesList; 
+        }
+
+       
+        public class PaisDisplay
+        {
+            public string DisplayText { get; set; }
+        }
+
+        
+        public class Pais
+        {
+            [PrimaryKey, AutoIncrement]
+            public int Id { get; set; }
+            public string NombreOficial { get; set; }
+            public string Region { get; set; }
+            public string GoogleMapsLink { get; set; }
+        }
+    }
 }
